a9211e6064894df6c20bdfc75f4ef443ca8e60b5
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2021-10-05 01:21:50 +0700

    show graph

--- backend/models/module.js
+++ backend/models/module.js
@@ -3,25 +3,24 @@ let fs = require('fs'),
 
 exports.genData = () => {
     fs.readFile('new_storage.json', (err, data) => {
-        // var data = new Set();
         var m = new Date().getMonth();
         usageModel.find((error, data) => {
             if (data.length == 0) {
-                for (let month = 7; month < m - 1; month++) {
+                var sum = 0;
+                for (let month = 8; month <= m + 1  ; month++) {
                     var lastDay = [2, 4, 6, 9, 11].includes(month) ? 30 : 31;
                     for (let day = 1; day <= lastDay; day++) {
                         var auDate = "2021-" + pad(month, 2) + "-" + pad(day, 2) + "T";
 
                         for (let hour = 0; hour < 24; hour++) {
                             for (let min = 0; min < 60; min += 15) {
-
+                                sum = sum + getRandomInt(20, 10)
                                 var auTime = pad(hour, 2) + ":" + pad(min, 2) + ":57.575Z";
                                 var obj = {
                                     time_stamp: auDate + auTime,
                                     sensor_id: 1,
-                                    electric_usage: getRandomInt(20, 10)
+                                    electric_usage: sum
                                 }
-                                // data.add(obj);
                                 usageModel(obj)
                                     .save()
                                     .catch((err) => {
--- backend/public/js/app.controller.js
+++ backend/public/js/app.controller.js
@@ -1,28 +1,21 @@
-(function (angular) {
-  'use strict';
-  angular.module('myApp', ['chart.js'])
-      .controller('myController', [function () {
-          var ctrl = this;
-          ctrl.socialChart = {
-              options: {
-                  scales: {
-                      xAxes: [{
-                          stacked: true,
-                      }],
-                      yAxes: [{
-                          stacked: true
-                      }]
-                  }
-              },
-              type: 'StackedBar',
-              labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-              series: ['FACEBOOK', 'GOOGLE', 'TWITTER', 'INSTAGRAM'],
-              colors: ['#ED402A', '#F0AB05', '#A0B421', '#00A39F'],
-              data: [
-                  [65, 59, 90, 81, 56, 55, 40],
-                  [28, 48, 40, 19, 96, 27, 100]
-              ]
-          }
-      }]);
+var app = angular.module("myApp", []);
 
-})(window.angular);
+const url = window.location.href;
+
+app.controller(
+  "ctrlGetData",
+  function (
+    $scope,
+    $http
+  ) {
+
+    $scope.getData1 = function () {
+      $http.get(`${url}api`).then(function (response) {
+          console.log(response);
+        $scope.data = response.data.data.result;
+      });
+    };
+
+    $scope.getData1();
+  }
+);
