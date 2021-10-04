0b8e69e7b6cda976272d29d7018c880a1111cf09
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2021-10-04 07:17:43 +0700

    create ui

--- backend/public/js/app.controller.js
+++ backend/public/js/app.controller.js
@@ -1,21 +1,28 @@
-var app = angular.module("myApp", []);
+(function (angular) {
+  'use strict';
+  angular.module('myApp', ['chart.js'])
+      .controller('myController', [function () {
+          var ctrl = this;
+          ctrl.socialChart = {
+              options: {
+                  scales: {
+                      xAxes: [{
+                          stacked: true,
+                      }],
+                      yAxes: [{
+                          stacked: true
+                      }]
+                  }
+              },
+              type: 'StackedBar',
+              labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+              series: ['FACEBOOK', 'GOOGLE', 'TWITTER', 'INSTAGRAM'],
+              colors: ['#ED402A', '#F0AB05', '#A0B421', '#00A39F'],
+              data: [
+                  [65, 59, 90, 81, 56, 55, 40],
+                  [28, 48, 40, 19, 96, 27, 100]
+              ]
+          }
+      }]);
 
-const url = window.location.href;
-
-app.controller(
-  "ctrlGetData",
-  function (
-    $scope,
-    $http
-  ) {
-
-    $scope.getData1 = function () {
-      $http.get(`${url}api`).then(function (response) {
-          console.log(response);
-        $scope.data = response.data.data.result;
-      });
-    };
-
-    $scope.getData1();
-  }
-);
+})(window.angular);
