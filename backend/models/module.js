3af3a148018a2750fb410734219eed6f686784f5
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2021-10-05 08:02:52 +0700

    modify layout

--- backend/models/module.js
+++ backend/models/module.js
@@ -2,38 +2,36 @@ let fs = require('fs'),
     usageModel = require('./Usage');
 
 exports.genData = () => {
-    fs.readFile('new_storage.json', (err, data) => {
-        var m = new Date().getMonth();
-        usageModel.find((error, data) => {
-            if (data.length == 0) {
-                var sum = 0;
-                for (let month = 8; month <= m + 1  ; month++) {
-                    var lastDay = [2, 4, 6, 9, 11].includes(month) ? 30 : 31;
-                    for (let day = 1; day <= lastDay; day++) {
-                        var auDate = "2021-" + pad(month, 2) + "-" + pad(day, 2) + "T";
+    var m = new Date().getMonth();
+    usageModel.find((error, data) => {
+        if (data.length == 0) {
+            var sum = 0;
+            for (let month = 8; month <= m + 1; month++) {
+                var lastDay = [2, 4, 6, 9, 11].includes(month) ? 30 : 31;
+                for (let day = 1; day <= lastDay; day++) {
+                    var auDate = "2021-" + pad(month, 2) + "-" + pad(day, 2) + "T";
 
-                        for (let hour = 0; hour < 24; hour++) {
-                            for (let min = 0; min < 60; min += 15) {
-                                sum = sum + getRandomInt(20, 10)
-                                var auTime = pad(hour, 2) + ":" + pad(min, 2) + ":57.575Z";
-                                var obj = {
-                                    time_stamp: auDate + auTime,
-                                    sensor_id: 1,
-                                    electric_usage: sum
-                                }
-                                usageModel(obj)
-                                    .save()
-                                    .catch((err) => {
-                                        console.log(err.message);
-                                    });
+                    for (let hour = 0; hour < 24; hour++) {
+                        for (let min = 0; min < 60; min += 15) {
+                            sum = sum + getRandomInt(20, 10)
+                            var auTime = pad(hour, 2) + ":" + pad(min, 2) + ":57.575Z";
+                            var obj = {
+                                time_stamp: auDate + auTime,
+                                sensor_id: 1,
+                                electric_usage: sum
                             }
+                            usageModel(obj)
+                                .save()
+                                .catch((err) => {
+                                    console.log(err.message);
+                                });
                         }
                     }
                 }
-                console.log("Success")
             }
-        })
-    });
+            console.log("Success")
+        }
+    })
 
     function pad(num, size) {
         num = num.toString();
