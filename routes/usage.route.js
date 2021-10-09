22eff1ef303a1c604e3f5a23b101d2c25a4e97b1
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2021-10-09 19:03:17 +0700

    [ADD] url weekly query with date

--- routes/usage.route.js
+++ routes/usage.route.js
@@ -9,8 +9,7 @@ usageRoute.route('/', usageController.render) // show Page index
 
 //get all data in db
 usageRoute.route('/api').get((req, res, next) => {
-  usageModel.find({ limit: 1 }, (error, data) => {
-    console.log(data);
+  usageModel.findOne((error, data) => {
     if (error) {
       return next(error)
     } else {
@@ -19,5 +18,25 @@ usageRoute.route('/api').get((req, res, next) => {
   })
 })
 
+
+// 2017-01-01T01:15:26.625Z
+
+// get data with date weekly
+//  https://[hostname]/api/timestamp?date=2019-12-12
+usageRoute.route("/api/weekly/timestamp").get(function (req, res, next) {
+  var reqDate = req.query.date
+  usageModel.find({
+    time_stamp: {
+      $gt: new Date(`${reqDate}T00:00:00.625z`),
+      $lte: new Date(`${reqDate}T23:55:00.625z`)
+    }
+  }, (error, data) => {
+    if (error) {
+      next(error)
+    } else {
+      res.json(data)
+    }
+  })
+});
 
 module.exports = usageRoute
