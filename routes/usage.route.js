7582550452511e758ee2821f17fd57e645b99607
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2021-10-10 00:40:17 +0700

    [EDIT] route query month

--- routes/usage.route.js
+++ routes/usage.route.js
@@ -18,15 +18,19 @@ usageRoute.route('/api').get((req, res, next) => {
   })
 })
 
-
+// 2017-01-01T00:00:26.625Z
 // get data with date weekly
 //  https://[hostname]/api/timestamp?date=2019-12-12
 usageRoute.route("/api/weekly/timestamp").get((req, res, next) => {
   var reqDate = req.query.date
+  var dateCon = new Date(reqDate)
+  var month = dateCon.getMonth() + 1
+  var year = dateCon.getFullYear()
+  console.log(year);
   usageModel.find({
     time_stamp: {
-      $gt: new Date(`${reqDate}T00:00:00.625z`),
-      $lte: new Date(`${reqDate}T23:55:00.625z`)
+      $gt: new Date(`${year}-${month}-01T00:00:26.625Z`),
+      $lte: new Date(`${year}-${month}-31T23:59:26.625Z`)
     }
   }, (error, data) => {
     if (error) {
